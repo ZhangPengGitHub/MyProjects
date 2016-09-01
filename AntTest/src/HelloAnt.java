51f97dcc7b18769de78bf9ac8f7bbb09664425df
ZhangPengGitHub <zp0016@qq.com>
2016-09-01 17:10:57 +0800

    Ant编译代码

---
+++ AntTest/src/HelloAnt.java
@@ -0,0 +1,13 @@
+/**
+ * AntTest
+ * 
+ * HelloAnt.java
+ * @author Roc
+ * @date 2016-9-1обнГ3:40:04
+ * @Email zp0016@qq.com
+ */
+public class HelloAnt {
+	public static void main(String[] args){
+		System.out.println("Hello Ant!");
+	}
+}
