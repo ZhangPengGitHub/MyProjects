bcfe0a98ce7d49f4e0dc3cfcdb73f6e130b8edc2
ZhangPengGitHub <zp0016@qq.com>
2016-12-06 16:28:00 +0800

    SpringContext

---
+++ SpringContext/src/main/java/com/maven/SpringContext/App.java
@@ -0,0 +1,13 @@
+package com.maven.SpringContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
